541d7e99cbb6f5420e580ee65555444eb58a4e49
Vlad Zhukov <vlad.zhukov24@gmail.com>
2023-03-06 11:43:59 +0300

    A virtual keyboard creation function has been created that is displayed during the process of adding a joke

--- src/keyboards/reply.py
+++ src/keyboards/reply.py
@@ -14,3 +14,13 @@ def main_menu_keyboard() -> types.ReplyKeyboardMarkup:
         resize_keyboard=True,
         input_field_placeholder="Выберите действие",
     )
+
+
+def adding_joke_keyboard() -> types.ReplyKeyboardMarkup:
+    """Create keyboard with button cancel"""
+    keyboard_buttons = [[types.KeyboardButton(text=Buttons.CANCEL.value)]]
+    return types.ReplyKeyboardMarkup(
+        keyboard=keyboard_buttons,
+        resize_keyboard=True,
+        input_field_placeholder="Введите анекдот",
+    )
