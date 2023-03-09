e17cb1c9f3c74d4b92e2b6e61124468d9a7b9043
Vladislav Zhukov <vlad.zhukov24@gmail.com>
2023-03-09 13:44:22 +0300

    changed user telegram id type from int to bigint added column "is_admin" in users renamed some columns

--- sql_queries/create_tables.sql
+++ sql_queries/create_tables.sql
@@ -5,16 +5,17 @@ CREATE TYPE "status" AS ENUM (
 );
 
 CREATE TABLE if not exists "users" (
-  "id" int UNIQUE PRIMARY KEY NOT NULL,
+  "user_telegram_id" bigint UNIQUE PRIMARY KEY NOT NULL,
   "nickname" text NOT NULL,
+  "is_admin" boolean DEFAULT FALSE,
   "created_at" timestamp NOT NULL
 );
 
 CREATE TABLE if not exists "jokes" (
-  "id" SERIAL UNIQUE PRIMARY KEY,
-  "status" status NOT NULL DEFAULT 'under_consideration',
-  "joke" text NOT NULL,
-  "author_id" int NOT NULL,
-  "created_at" timestamp NOT NULL
+  "joke_id" SERIAL UNIQUE PRIMARY KEY,
+  "publication_status" status DEFAULT 'under_consideration',
+  "joke_text" text NOT NULL,
+  "author_telegram_id" bigint NOT NULL,
+  "creation_date" timestamp NOT NULL
 );
-ALTER TABLE "jokes" ADD FOREIGN KEY (author_id) REFERENCES "users" ("id");
+ALTER TABLE "jokes" ADD FOREIGN KEY ("author_telegram_id") REFERENCES "users" ("user_telegram_id");
