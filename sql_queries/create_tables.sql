43762b63a81bc892379c69f0f85b4fa0a8513706
Vladislav Zhukov <vlad.zhukov24@gmail.com>
2023-03-10 20:35:46 +0300

    renamed column "nickname" to "telegram_nickname" in table "users" renamed column "created_at" to "registration_date" in table "users"

--- sql_queries/create_tables.sql
+++ sql_queries/create_tables.sql
@@ -6,9 +6,9 @@ CREATE TYPE "status" AS ENUM (
 
 CREATE TABLE if not exists "users" (
   "user_telegram_id" bigint UNIQUE PRIMARY KEY NOT NULL,
-  "nickname" text NOT NULL,
+  "telegram_nickname" text NOT NULL,
   "is_admin" boolean DEFAULT FALSE,
-  "created_at" timestamp NOT NULL
+  "registration_date" timestamp NOT NULL
 );
 
 CREATE TABLE if not exists "jokes" (
