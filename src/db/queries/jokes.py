a3d7d235aaa2fec15b7d4b0e3bd7354f8d416430
Vladislav Zhukov <vlad.zhukov24@gmail.com>
2023-03-10 21:52:51 +0300

    changed columns names in database queries

--- src/db/queries/jokes.py
+++ src/db/queries/jokes.py
@@ -6,7 +6,9 @@ from src.db.connector import Connector
 def add_joke(connector: Connector, joke: str, user_id: int):
     """Add joke in jokes table"""
     cursor = connector.get_cursor()
-    query = """INSERT INTO jokes (joke, author_id, created_at)
+    query = """INSERT INTO jokes (
+                 joke_text, author_telegram_id, creation_date
+               )
                VALUES
                  (%s, %s, %s)"""
     cursor.execute(query, (joke, user_id, datetime.now()))
