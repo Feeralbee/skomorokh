1728cc43e69cf514ef43897303ce0627b5575c04
Vladislav Zhukov <vlad.zhukov24@gmail.com>
2023-03-10 18:48:38 +0300

    an SQL query procedure has been created to check whether the user is an admin

--- src/db/queries/users.py
+++ src/db/queries/users.py
@@ -17,3 +17,17 @@ def add_user(connector: Connector, user_id: int, user_name: str):
                  telegram_nickname = %s"""
     cursor.execute(query, (user_id, user_name, datetime.now(), user_name))
     connector.connection.commit()
+
+
+def checks_user_is_admin(connector: Connector, user_id: int) -> bool:
+    """Checks user is admin"""
+    cursor = connector.get_cursor()
+    query = """SELECT
+                 is_admin
+               FROM
+                 users
+               WHERE
+                 user_telegram_id = %s"""
+    cursor.execute(query, (user_id,))
+    (result,) = cursor.fetchone()
+    return result
