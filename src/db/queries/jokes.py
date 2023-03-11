a646a60d724578805a9ee653341874d52f330f16
Vladislav Zhukov <vlad.zhukov24@gmail.com>
2023-03-11 19:03:10 +0300

    A database query processing function has been created to get the joke_id and joke_text of the joke from the jokes table, which is under consideration

--- src/db/queries/jokes.py
+++ src/db/queries/jokes.py
@@ -4,7 +4,7 @@ from src.db.connector import Connector
 
 
 def add_joke(connector: Connector, joke: str, user_id: int):
-    """Add joke in jokes table"""
+    """Adds joke in jokes table"""
     cursor = connector.get_cursor()
     query = """INSERT INTO jokes (
                  joke_text, author_telegram_id, creation_date
@@ -13,3 +13,21 @@ def add_joke(connector: Connector, joke: str, user_id: int):
                  (%s, %s, %s)"""
     cursor.execute(query, (joke, user_id, datetime.now()))
     connector.connection.commit()
+
+
+def get_under_consideration_joke(connector: Connector):
+    """Gets the joke_id and joke_text of the joke from the jokes table, which is under review"""
+    cursor = connector.get_cursor()
+    query = """SELECT
+                joke_id,
+                joke_text
+               FROM
+                jokes
+               WHERE
+                publication_status='under_consideration'
+               ORDER BY
+                joke_id
+               LIMIT
+                1"""
+    cursor.execute(query)
+    return cursor.fetchone()
