7e18b5ec667f770808754e9fb2a409ac7e04a115
Vladislav Zhukov <vlad.zhukov24@gmail.com>
2023-03-11 18:44:59 +0300

    Added new buttons for virtual keyboards Created inline virtual keyboard creator for admins to managing new jokes

---
+++ src/keyboards/inline.py
@@ -0,0 +1,21 @@
+"""Creating inline keyboards"""
+from aiogram import types
+from src.keyboards.buttons import Buttons
+
+
+def get_managing_new_jokes_keyboard() -> types.InlineKeyboardMarkup:
+    """Creates inline virtual keyboard for admins to managing new jokes"""
+    keyboard_buttons = [
+        [
+            types.InlineKeyboardButton(
+                text=Buttons.CHECK_MARK_BUTTON.value, callback_data="approve"
+            ),
+            types.InlineKeyboardButton(
+                text=Buttons.CROSS_MARK_BUTTON.value, callback_data="disapprove"
+            ),
+        ]
+    ]
+    return types.InlineKeyboardMarkup(
+        keyboard=keyboard_buttons,
+        resize_keyboard=True,
+    )
