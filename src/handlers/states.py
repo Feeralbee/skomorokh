0fdf253291d3306b689da06e18831ef4dd410c85
Vladislav Zhukov <vlad.zhukov24@gmail.com>
2023-03-14 22:10:20 +0300

    created new StateGroup class ManagingNewJokes

--- src/handlers/states.py
+++ src/handlers/states.py
@@ -8,3 +8,11 @@ class AddingJoke(StatesGroup):
     """States for adding joke"""
 
     inputting_joke_text = State()
+
+
+@dataclass
+class ManagingNewJokes(StatesGroup):
+    """States for Managing new joke"""
+
+    joke_id: int
+    choosing_an_action = State()
