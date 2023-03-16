30700b7d0d578f1ee313ab8797db3a0aa5708565
Vladislav Zhukov <vlad.zhukov24@gmail.com>
2023-03-16 21:21:02 +0300

    Added a "/managing_new_jokes" command handler Added a "/stop_managing" command handler Added a "approve" callback data handler Added a "disapprove" callback data handler Added procedure of registration admin handlers to dispatcher

---
+++ src/misc/utils.py
@@ -0,0 +1,36 @@
+"""Utils"""
+from aiogram.fsm.context import FSMContext
+from aiogram.types import Message, CallbackQuery
+from src.keyboards import inline
+from src.db.queries.jokes import get_under_consideration_joke, update_publication_status
+from src.keyboards import reply
+from src.db.connector import Connector
+from src.handlers.states import ManagingNewJokes
+
+
+async def manage_jokes(
+    message: Message, state: FSMContext, dbconnector: Connector
+) -> None:
+    """Manages jokes"""
+    data = get_under_consideration_joke(dbconnector)
+    if data is None:
+        await message.answer(
+            "Новых анекдотов не найдено", reply_markup=reply.main_menu_keyboard()
+        )
+        await state.clear()
+    else:
+        joke_id, joke_text = data
+        await message.answer(
+            joke_text, reply_markup=inline.get_managing_new_jokes_keyboard()
+        )
+        await state.set_state(ManagingNewJokes.choosing_an_action)
+        await state.update_data(joke_id=joke_id)
+
+
+async def set_publication_status(
+    call: CallbackQuery, state: FSMContext, dbconnector: Connector, status: str
+) -> None:
+    """Sets publication status"""
+    data = await state.get_data()
+    update_publication_status(dbconnector, data["joke_id"], status)
+    await call.message.edit_reply_markup(reply_markup=None)
